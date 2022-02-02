8e71c1cd070a829ac0a18b976aae03f248d5b48a
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-02-02 22:08:56 +0200

    Add editable content with no null validation and max length

--- static/js/controller/page_actions.js
+++ static/js/controller/page_actions.js
@@ -1,5 +1,19 @@
-document.addEventListener('click', e => {
-    if (e.target.id === 'id01' || e.target.id === 'id02') {
-        e.target.style = 'none'
+let defaultText = null;
+
+addEventListener('click', event => {
+
+    if (event.target.id === 'id01' || event.target.id === 'id02') {
+        event.target.style = 'none'
+    }
+
+    if (event.target.isContentEditable) {
+        defaultText = event.target.innerHTML
+
+        event.target.addEventListener('blur', e => {
+
+            if (event.target.innerHTML == '') {
+                event.target.innerHTML = defaultText
+            }
+        })
     }
 })
--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -17,12 +17,11 @@ export function htmlFactory(template) {
 
 function boardBuilder(board) {
     return `<div class="board-container">
-                <div class="board" data-board-id=${board.id}>${board.title}</div>
+                <div class="board" data-board-id=${board.id} ><p id="board-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${board.title}</p></div>
                 <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
             </div>`;
 }
 
 function cardBuilder(card) {
-    return `<div class="card" data-card-id="${card.id}">${card.title}</div>`;
+    return `<div class="card" data-card-id="${card.id}"><p id="card-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${card.title}</p></div>`;
 }
-
