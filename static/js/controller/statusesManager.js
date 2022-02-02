c43e8e149c862b45ff3d0f4084bc999d5346f7b5
dragosvasile11 <vasile.dragos11@gmail.com>
2022-02-03 01:26:37 +0200

    adding statuses as children of boards, using import domManger

--- static/js/controller/statusesManager.js
+++ static/js/controller/statusesManager.js
@@ -1,11 +1,14 @@
 import { dataHandler } from "../data/dataHandler.js";
 import { htmlFactory, htmlTemplates } from "../view/htmlFactory.js";
+import { domManager } from "../view/domManager.js";
 
 export let statusesManager = {
-    loadStatuses: async function () {
+    loadStatuses: async function (boardId) {
         const statuses = await dataHandler.getStatuses();
         for (let status of statuses) {
-            const statusBuilder = htmlFactory(htmlTemplates.status)
+            const statusBuilder = htmlFactory(htmlTemplates.status);
+            const content = statusBuilder(status);
+            domManager.addChild(`.board[data-board-id="${boardId}"]`, content);
         }
     }
 }
