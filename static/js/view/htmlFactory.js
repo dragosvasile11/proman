048ad2b485ae5d8384df6042ca396efbe027764e
dragosvasile11 <vasile.dragos11@gmail.com>
2022-02-03 00:15:47 +0200

    created statusBuilder function in the htmlFactory for creating columns for cards

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -1,6 +1,7 @@
 export const htmlTemplates = {
     board: 1,
-    card: 2
+    card: 2,
+    status: 3
 }
 
 export function htmlFactory(template) {
@@ -9,6 +10,8 @@ export function htmlFactory(template) {
             return boardBuilder
         case htmlTemplates.card:
             return cardBuilder
+        case htmlTemplates.status:
+            return statusBuilder
         default:
             console.error("Undefined template: " + template)
             return () => { return "" }
@@ -25,13 +28,23 @@ function boardBuilder(board) {
 }
 
 function cardBuilder(card) {
-    return `<div class="card" data-card-id="${card.id}" data-board-id="${card.board_id}"><p id="card-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${card.title}</p></div>`;
+    return `<div class="card" data-card-id="${card.id}" data-board-id="${card.board_id}"><span class="card-title"><p id="card-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${card.title}</p></span>
+                <div class="card-remove"><i class="fas fa-trash-alt"></i></div>
+                <div class="card-title" data-board-id="${card.board_id}" data-card-id="${card.id}"></div>
+            </div>`
 }
 
-
-
+function statusBuilder(status) {
+    return `<div class="board-column">
+                <div class="board-column-title">New</div>
+                <div class="board-column-content"></div>
+            </div>`
+}
 
  // `<div class="board-container">
  //                <div class="board" data-board-id=${board.id} ><p id="board-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${board.title}</p></div>
  //                <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
  //            </div>`;
+
+
+// `<div class="card" data-card-id="${card.id}" data-board-id="${card.board_id}"><p id="card-title" contenteditable="true" onkeypress="return (this.innerText.length <= 10)">${card.title}</p></div>`;
