88b213c883e6b6bbf3efe4784e3c4359721106e2
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-02-04 14:26:15 +0200

    Add board id for each status

--- data/proman_sample_data.sql
+++ data/proman_sample_data.sql
@@ -35,7 +35,8 @@ CREATE TABLE users (
 
 CREATE TABLE statuses (
     id       SERIAL PRIMARY KEY     NOT NULL,
-    title    VARCHAR(200)           NOT NULL
+    title    VARCHAR(200)           NOT NULL,
+    board_id INTEGER                NOT NULL
 );
 
 CREATE TABLE boards (
@@ -59,26 +60,30 @@ CREATE TABLE cards (
 
 INSERT INTO users(username, password) VALUES ('codecool@mail.com', 'pbkdf2:sha256:260000$vyuEe9q7l5gJqr1V$0ff7768c7f0feb01ca24470086876a72e437ce98b428b8ee0d3c32ed752d7606');
 
-INSERT INTO statuses(title) VALUES ('new');
-INSERT INTO statuses(title) VALUES ('in progress');
-INSERT INTO statuses(title) VALUES ('testing');
-INSERT INTO statuses(title) VALUES ('done');
+INSERT INTO statuses(title, board_id) VALUES ('new', 1);
+INSERT INTO statuses(title, board_id) VALUES ('in progress', 1);
+INSERT INTO statuses(title, board_id) VALUES ('testing', 1);
+INSERT INTO statuses(title, board_id) VALUES ('done', 1);
+INSERT INTO statuses(title, board_id) VALUES ('new', 2);
+INSERT INTO statuses(title, board_id) VALUES ('in progress', 2);
+INSERT INTO statuses(title, board_id) VALUES ('testing', 2);
+INSERT INTO statuses(title, board_id) VALUES ('done', 2);
 
 INSERT INTO boards(user_id, title, public) VALUES (1, 'Board 1', TRUE);
 INSERT INTO boards(user_id, title, public) VALUES (1, 'Board 2', TRUE);
 
 INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 2, 'in progress card', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 3, 'planning', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 2, 'in progress card', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 3, 'planning', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 2);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 2', 1);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 2, 'in progress card', 2);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 3, 'planning', 2);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 4);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 4);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 1', 5);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 2', 5);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 2, 'in progress card', 6);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 3, 'planning', 6);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 8);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 8);
 
 ---
 --- add constraints
@@ -92,3 +97,6 @@ ALTER TABLE ONLY cards
 
 ALTER TABLE ONLY boards
     ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id);
+
+ALTER TABLE ONLY statuses
+    ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards (id);
