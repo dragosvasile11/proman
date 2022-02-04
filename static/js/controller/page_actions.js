f1f7bad087dcee768277afd58a7138791482e8e6
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-02-04 07:20:28 +0200

    Toggle show/hide board content

--- static/js/controller/page_actions.js
+++ static/js/controller/page_actions.js
@@ -2,6 +2,17 @@ let defaultText = null;
 
 addEventListener('click', event => {
 
+    if (event.target.className === 'board-toggle hidden') {
+        let boardId = event.target.dataset.boardId;
+        let boardContent = document.querySelector(`.board-columns[data-board-id="${boardId}"]`)
+        if (boardContent.hidden === true) {
+            boardContent.hidden = false; 
+        }else {
+            boardContent.hidden = true;
+        }
+    }
+
+
     if (event.target.id === 'id01' || event.target.id === 'id02') {
         event.target.style = 'none'
     }
